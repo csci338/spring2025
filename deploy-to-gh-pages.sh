5b32722ea66dffb2384863b01c6873de7f5185a0
Sarah <svanwart@unca.edu>
2025-01-11 14:18:00 -0500

    Removing old script and adding output messages

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -1,10 +1,16 @@
 # build the site and copy the files to a temporary directory:
-rvm use 2.7 && bundle exec jekyll build
+echo "Building site..."
+bundle exec jekyll build
+
+echo "Copying files into temp directory..."
 TEMP_DIR=$(mktemp -d)
 rsync -av --exclude='node_modules' --exclude='*.pyc' --exclude='*.md' _site/ "$TEMP_DIR"
 
 # checkout gh-pages and remove all files:
+echo "Checking out gh-pages branch..."
 git checkout gh-pages
+
+echo "Removing existing files..."
 git rm -rf .
 rm -r _site
 rm -r .sass-cache
@@ -13,6 +19,7 @@ rm Gemfile
 rm .gitignore
 
 # create .gitignore file to exclude unnecessary files
+echo "Creating .gitignore to exclude unnecessary files..."
 echo "_site
 .sass-cache
 *.sh
@@ -23,15 +30,20 @@ Gemfile.lock
 
 
 # copy the new site files to the gh-pages branch:
+echo "Copying files from the temp directory into gh-pages branch..."
 cp -r "$TEMP_DIR/" .
 
 
 # commit changes and send them to GitHub:
+echo "Adding, committing, and pushing to GitHub..."
 git add .
 git commit -m 'Updated gh-pages with new site content'
 git push -f origin gh-pages
+echo "New files pushed!"
 
 # clean up:
+echo "Restoring main and cleaning up..."
 git checkout main
 rm -rf _site
 rm -rf "$TEMP_DIR"
+echo "The main branch is restored and the temporary directory is cleaned up."
