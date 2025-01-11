8a5500058edf493f8d673f9d88a0f1203d8fac60
Sarah <svanwart@unca.edu>
2025-01-11 13:59:54 -0500

    Added new build script

---
+++ deploy-to-gh-pages.sh
@@ -0,0 +1,38 @@
+# build the site and copy the files to a temporary directory:
+rvm use 2.7 && bundle exec jekyll build
+TEMP_DIR=$(mktemp -d)
+rsync -av --exclude='node_modules' --exclude='*.pyc' _site/ "$TEMP_DIR"
+
+# checkout gh-pages and remove all files:
+git checkout gh-pages
+git rm -rf .
+rm -r _site
+rm -r .sass-cache
+rm Gemfile.lock
+rm Gemfile
+rm .gitignore
+
+# create .gitignore file to exclude unnecessary files
+echo "_site
+.sass-cache
+*.md
+*.sh
+*.yml
+Gemfile
+Gemfile.lock
+" > .gitignore
+
+
+# copy the new site files to the gh-pages branch:
+cp -r "$TEMP_DIR/" .
+
+
+# commit changes and send them to GitHub:
+git add .
+git commit -m 'Updated gh-pages with new site content'
+git push -f origin gh-pages
+
+# clean up:
+git checkout main
+rm -rf _site
+rm -rf "$TEMP_DIR"
