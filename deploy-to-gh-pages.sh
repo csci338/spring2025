3d6cdfa5bfe0b210309ac7b2490138edbb2c145b
Sarah <svanwart@unca.edu>
2025-03-04 14:35:01 -0500

    WIP: Editing deploy file

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -1,16 +1,25 @@
 # build the site and copy the files to a temporary directory:
 echo "Building site..."
 bundle exec jekyll build
+if [ $? -ne 0 ]; then
+    echo "Site build failed"
+    exit 1
+fi
 
 echo "Copying files into temp directory..."
 TEMP_DIR=$(mktemp -d)
 rsync -av --exclude='node_modules' --exclude='*.pyc' --exclude='*.md' _site/ "$TEMP_DIR"
+if [ $? -ne 0 ]; then
+    echo "Failed to copy files to temporary directory"
+    rm -rf "$TEMP_DIR"
+    exit 1
+fi
 
 # checkout gh-pages and remove all files:
 echo "Checking out gh-pages branch..."
 git checkout gh-pages
 
-echo "Removing existing files..."
+# echo "Removing existing files..."
 git rm -rf .
 rm -r _site
 rm -r .sass-cache
@@ -31,15 +40,15 @@ Gemfile.lock
 
 # copy the new site files to the gh-pages branch:
 echo "Copying files from the temp directory into gh-pages branch..."
-cp -r "$TEMP_DIR/" .
+cp -r "$TEMP_DIR"/* .
 
 
-# commit changes and send them to GitHub:
+# commit changes and send them to GitHub
 echo "Adding, committing, and pushing to GitHub..."
 git add .
 git commit -m 'Updated gh-pages with new site content'
 git push -f origin gh-pages
-echo "New files pushed!"
+echo "New files pushed"
 
 # clean up:
 echo "Restoring main and cleaning up..."
